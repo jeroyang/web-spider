e52978075ec2c8e4b98f3aa87a4c3ea32ca9c686
jeroyang <jeroyang@gmail.com>
2012-11-01 00:29:51 +0800

    Add some import seeds

--- settings.py
+++ settings.py
@@ -9,7 +9,13 @@ URL_QUEUE_SIZE = 20000
 
 REPORT_INTERVAL = 20 # The interval between two reports about fetched pages, and fetching speed.
 EXCLUDE = r'(?i).*?/.*?\.(gz|tar|tgz|z|zip|exe|ps|doc|pdf|xplot|java|c|h|txt|ppt|gif|tiff|tif|png|jpeg|jpg|jpe|xls|map)$'
-SEEDS = ['http://www.dmoz.org/', 'http://web.ncku.edu.tw/bin/home.php', 'http://dir.yahoo.com/'] # will be used whenever the seeds.txt is not exist or empty
+SEEDS = ['http://www.dmoz.org/', 
+        'http://web.ncku.edu.tw/bin/home.php', 
+        'http://dir.yahoo.com/', 
+        'http://tw.yahoo.com', 
+        'http://www.usa.gov/',
+        'http://australia.gov.au/',
+        'http://vlib.org/'] # will be used whenever the seeds.txt is not exist or empty
 SAMPLING_SIZE = 400 # greater the number, more memmory will be used, lesser the number, increase the possibility of out_of_url
 TIME_LIMIT = 30*60 # in seconds
 
